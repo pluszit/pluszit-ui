71cd5f40f3094023af257b168375782414a6e4c2
pluszkiewicz <pluszkiewicz@gmail.com>
2023-02-01 20:22:09 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MDEditor from '@uiw/react-md-editor';
 
 function App() {
+  const [value, setValue] = React.useState("");
+
+  const changeText = (value: any) => {
+    setValue(value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <MDEditor height={200} value={value} onChange={changeText} />
     </div>
   );
 }
